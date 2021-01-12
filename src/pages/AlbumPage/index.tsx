a55b7986d4e39d3818635fca0a90fbc359b7c91b
Gustavo Cauzzi <gustavocauzzi@gmail.com>
2021-01-12 16:07:41 -0300

    AlbumPage: Frontend finished

--- src/pages/AlbumPage/index.tsx
+++ src/pages/AlbumPage/index.tsx
@@ -1,9 +1,28 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRoute } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import IconAntDesing from 'react-native-vector-icons/AntDesign';
+import IconFontisto from 'react-native-vector-icons/Fontisto';
 
-import { Text } from 'react-native';
-
-import { Container } from './styles';
+import { 
+  AlbumCover,
+  AlbumInfo,
+  AlbumInfoContainer,
+  AlbumName,
+  AlbumNameTicker,
+  ArtistName,
+  ArtistNameTicker,
+  Container,
+  Content,
+  GoBackContainer, 
+  SongAlbumCover, 
+  SongAlbumCoverPlaceHolder, 
+  SongContainer,
+  SongInfo,
+  SongName,
+  SongNameTicker
+} from './styles';
+import { FlatList, Text, View } from 'react-native';
+import { useSongs } from '../../hooks/songs';
 
 interface Album {
   album: {
@@ -13,19 +32,151 @@ interface Album {
   artist: string;
 }
 
-const AlbumPage: React.FC = () => {
-  const [currentAlbum, setCurrentAlbum] = useState<Album>({} as Album);
+interface MusicFile{
+  id: number,
+  title: string,
+  author: string,
+  album: string,
+  duration : number,
+  cover: string | undefined,
+  path: string
+}
+
+interface AlbumPageProps {
+  navigation?: any;
+}
+
+const AlbumPage: React.FC<AlbumPageProps> = ({ navigation }) => {
+  const [currentAlbum, setCurrentAlbum] = useState<Album>();
+  const [currentSongs, setCurrentSongs] = useState<MusicFile[]>([] as MusicFile[]);
+  const { songList, playSong } = useSongs();
+
+  const maxAlbumName = 13;
+  const maxArtistName = 26;
+  const maxSongName = 34;
 
   const route = useRoute();
   let routeParams = route.params as Album;
 
   useEffect(() => {
-    console.log(routeParams);
-    setCurrentAlbum(routeParams);
+    routeParams = route.params as Album;
+    setCurrentAlbum(routeParams as Album);
+
+    if(routeParams != undefined){
+      const songsFromAlbum = songList.filter(s => s.author.toLowerCase() == routeParams.artist.toLowerCase() && s.album.toLowerCase() == routeParams.album.name!.toLowerCase());
+      console.log('songsFromAlbum: ',songsFromAlbum);
+      setCurrentSongs(songsFromAlbum);
+    }
   }, [route.params]);
 
+  const handleGoBack = useCallback(() => {
+    navigation.goBack();
+  }, []);
+
+  const handlePlayMusic = useCallback((song: any) => {
+    playSong(song);    
+
+    const {id, title, path, author, cover, duration, album} = song;
+    
+    navigation.jumpTo('Player', {
+      id,
+      title,
+      path,
+      author,
+      cover,
+      duration,
+      album,
+    })
+  }, []);
+  
   return (
     <Container>
+      <GoBackContainer onPress={handleGoBack}>
+        <IconAntDesing name="arrowleft" size={20} color="#fff"/>
+      </GoBackContainer>
+      <AlbumInfoContainer>
+        <AlbumInfo>
+          {
+            currentAlbum != undefined
+              ? <AlbumCover source={{uri: currentAlbum.album.cover}}/>
+              : null
+          }
+          {
+            currentAlbum != undefined && currentAlbum.album.name && currentAlbum.artist
+              ? (
+                <View style={{marginLeft: 10}}>
+                  {currentAlbum.album.name 
+                    ? currentAlbum.album.name.length <= maxAlbumName
+                      ? <AlbumName>{currentAlbum.album.name}</AlbumName>
+                      : (
+                        <AlbumNameTicker
+                          duration={15000}
+                          repeatSpacer={50}
+                          marqueeDelay={1000}
+                        > 
+                          {currentAlbum.album.name}
+                        </AlbumNameTicker>
+                      )
+                    : <AlbumName>Desconhecido</AlbumName>
+                  }
+                  {
+                    currentAlbum.artist
+                      ? currentAlbum.artist.length <= maxArtistName
+                        ? <ArtistName>{currentAlbum.artist}</ArtistName>
+                        : (
+                          <ArtistNameTicker
+                            duration={15000}
+                            repeatSpacer={50}
+                            marqueeDelay={1000}
+                          >
+                            {currentAlbum.artist}
+                          </ArtistNameTicker>
+                        )
+                      : <ArtistName>Desconhecido</ArtistName>
+                  }
+                </View>
+              )
+              : null
+          }
+        </AlbumInfo>
+      </AlbumInfoContainer>
+      <Content>
+        <FlatList
+          data={currentSongs}
+          maxToRenderPerBatch={30}
+          keyExtractor={(song) => String(song.id)}
+          getItemLayout={(_, index) => (
+            { length: 70, offset: 70 * index, index }
+          )}
+          renderItem={({item: song}) => (
+            <SongContainer key={song.id} onPress={() => {handlePlayMusic(song)}}>
+              {song.cover 
+                ? <SongAlbumCover source={{uri: `${song.cover}`}}/>
+                : (
+                  <SongAlbumCoverPlaceHolder>
+                    <IconFontisto name="music-note" color="#fff" size={20}/>    
+                  </SongAlbumCoverPlaceHolder>
+                )
+              }
+              <SongInfo>
+                {song.title && song.title.length <= maxSongName
+                  ? <SongName>{song.title}</SongName>
+                  : (
+                    <SongNameTicker
+                    duration={15000}
+                    repeatSpacer={50}
+                    marqueeDelay={1000}
+                    >
+                      {song.title}
+                    </SongNameTicker>
+                  )
+                }
+                <ArtistName>{song.author != '<unknown>' ? song.author : 'Desconhecido'}</ArtistName>
+              </SongInfo>
+            </SongContainer>
+          )}
+        />
+      </Content>
     </Container>
   );
 };
--- src/pages/AlbumPage/styles.ts
+++ src/pages/AlbumPage/styles.ts
@@ -1,4 +1,5 @@
 import { RectButton } from 'react-native-gesture-handler';
+import TextTicker from 'react-native-text-ticker';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -6,19 +7,103 @@ export const Container = styled.View`
   background-color: #111;
 `;
 
-// export const GoBackContainer = styled(RectButton)<GoBackContainerProps>`
-//   position: absolute;
-//   top: 10px;
-//   left: 10px;
-//   opacity: 0.9;
-//   background-color: #222;
-//   width: 35px;
-//   height: 35px;
-//   align-items: center;
-//   justify-content: center;
-//   border-radius: 17.5px;
-//   z-index: 10;
-//   /* transition: opacity 0.2s; */
+export const AlbumInfoContainer = styled.View`
+  height: 300px;
+  width: 100%;
+  padding: 30px 0px;
+  padding-top: 100px;
+`;
 
-//   ${props => !props.isVisible ? css`opacity: 0` : css`opacity: 0.8`}
-// `;
+export const AlbumInfo = styled.View`
+  flex: 1;
+  align-items: center;
+  flex-direction: row;
+  padding-left: 20px;
+`;
+
+export const AlbumCover = styled.Image`
+  width: 150px;
+  height: 150px;
+  border-radius: 20px;
+`;
+
+export const AlbumNameTicker = styled(TextTicker)`
+  color: #e5e5e5;
+  font-size: 25px;
+`;
+
+export const ArtistNameTicker = styled(TextTicker)`
+  color: #aaa;
+  font-size: 12px;
+`;
+
+export const AlbumName = styled.Text`
+  color: #e5e5e5;
+  font-size: 25px;
+`;
+
+export const ArtistName = styled.Text`
+  color: #aaa;
+  font-size: 12px;
+`;
+
+export const GoBackContainer = styled(RectButton)`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  opacity: 0.9;
+  background-color: #222;
+  width: 35px;
+  height: 35px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 17.5px;
+  z-index: 10;
+`;
+
+export const SongContainer = styled(RectButton)`
+  background-color: #1a1a1a;
+  margin: 5px 0;
+  padding: 5px 5px;
+  flex-direction: row;
+  border-radius: 5px;
+  align-items: center;
+`;
+
+export const SongAlbumCover = styled.Image`
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+  border-radius: 5px;
+`;
+
+export const SongAlbumCoverPlaceHolder = styled.View`
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  background-color: #252525;
+`;
+
+export const SongInfo = styled.View`
+`;
+
+export const SongName = styled.Text`
+  color: #e5e5e5;
+`;
+
+export const SongArtistName = styled.Text`
+  color: #a5a5a5;
+  font-size: 12px;
+`;
+
+export const Content = styled.View`
+  padding: 0px 10px;
+  margin-bottom: 25px;
+`;
+
+export const SongNameTicker = styled(TextTicker)`
+  color: #e5e5e5;
+`;
